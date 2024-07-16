7ebbd6ac3ce976a6885ca9ac6cc763429164dd39
unknown <kumargauravtiwary@gmail.com>
2024-07-16 11:35:25 +0530

    Update Jekyll site

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,24 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1721108897',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
